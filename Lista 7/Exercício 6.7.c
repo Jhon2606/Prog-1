88c71e331f0146a2fc047c1abdd6cdf62a18ef4c
Jhonatas Vinicius Neri Dos Santos <94151809+Jhon2606@users.noreply.github.com>
2023-06-26 10:56:57 -0300

    Add files via upload

---
+++ Lista 7/Exercício 6.7.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void troca(int *a, int *b);
+int main (){
+
+    int x, y;
+
+    printf("Digite o valor de x:\n");
+    scanf("%d", &x);
+    printf("Digite o valor de y:\n");
+    scanf("%d", &y);
+
+    troca(&x, &y);
+
+    printf("O valor trocado x e:%d e o de y e:%d\n", x, y);
+
+    return 0;
+}
+void troca(int *a, int *b){
+    int c = 0;
+
+    c = *a;
+    *a = *b;
+    *b = c;
+
+    return c;
+}
