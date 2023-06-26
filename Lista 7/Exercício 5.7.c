61df1668915db5a6224cbe5d96942bc40718a7d4
Jhonatas Vinicius Neri Dos Santos <94151809+Jhon2606@users.noreply.github.com>
2023-06-26 10:56:41 -0300

    Add files via upload

---
+++ Lista 7/Exercício 5.7.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+float duplicador( float a);
+int main(){
+
+    float n1, n2, n3;
+
+    printf("Digite o primeiro numero:\n");
+    scanf("%f", &n1);
+    printf("Digite o primeiro numero:\n");
+    scanf("%f", &n2);
+    printf("Digite o primeiro numero:\n");
+    scanf("%f", &n3);
+
+    printf("O dobro do primeiro e:%.f\n", duplicador(n1));
+    printf("O dobro do segundo e:%.f\n", duplicador(n2));
+    printf("O dobro do terceiro e:%.f\n", duplicador(n3));
+
+
+    return 0;
+}
+float duplicador( float a){
+
+    float b;
+
+    b = 2 * a;
+
+    return b;
+}
