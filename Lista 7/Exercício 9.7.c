b13991e16203a3bb51394fb13b7882bd2420aea1
Jhonatas Vinicius Neri Dos Santos <94151809+Jhon2606@users.noreply.github.com>
2023-06-29 11:46:19 -0300

    Exercício 9.7.c

--- Lista 7/Exercício 9.7.c
+++ Lista 7/Exercício 9.7.c
@@ -1,18 +1,5 @@
 #include <stdio.h>
 
-
-int main(){
-
-    int n;
-
-    printf("Digite um valor:\n");
-    scanf("%d", &n);
-
-    SINAL(n);
-
-
-    return 0;
-}
 void SINAL(int N){
 
     if( N < 0){
